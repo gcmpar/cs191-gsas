9eefe197bd6f380d0061affc444e671d9b066223
Glenn <gmparagas1@up.edu.ph>
2025-11-05 11:54:11 +0800

    feat(student_profiles): added autogenerated ORM

--- student_profiles/models.py
+++ student_profiles/models.py
@@ -1,3 +1,160 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.IntegerField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.IntegerField()
+    is_active = models.IntegerField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class Courses(models.Model):
+    course_code = models.CharField(max_length=20, blank=True, null=True)
+    description = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        db_table = 'courses'
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        db_table = 'django_session'
+
+
+class Prereqs(models.Model):
+    program = models.CharField(max_length=14)
+    core_course_code = models.CharField(max_length=50)
+    prereq_course_code = models.CharField(max_length=50)
+    description = models.CharField(max_length=1000)
+
+    class Meta:
+        db_table = 'prereqs'
+
+
+class Student(models.Model):
+    idstudent = models.AutoField(db_column='idStudent', primary_key=True)  # Field name made lowercase.
+    student_name = models.CharField(max_length=100)
+    email = models.CharField(unique=True, max_length=50, blank=True, null=True)
+    password = models.CharField(max_length=255, blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    phone_num = models.CharField(max_length=12, blank=True, null=True)
+    sex = models.CharField(max_length=6, blank=True, null=True)
+    birthdate = models.CharField(max_length=10, blank=True, null=True)
+    university = models.CharField(max_length=255, blank=True, null=True)
+    years_attended = models.CharField(max_length=20, blank=True, null=True)
+    degree_title = models.CharField(max_length=255, blank=True, null=True)
+    extracted_text = models.TextField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'student'
+
+
+class Transcripts(models.Model):
+    student = models.ForeignKey(Student, models.DO_NOTHING)
+    semester = models.CharField(max_length=50, blank=True, null=True)
+    academic_year = models.CharField(max_length=20, blank=True, null=True)
+    course = models.ForeignKey(Courses, models.DO_NOTHING, blank=True, null=True)
+    grade = models.CharField(max_length=10, blank=True, null=True)
+    units = models.CharField(max_length=10, blank=True, null=True)
+
+    class Meta:
+        db_table = 'transcripts'
