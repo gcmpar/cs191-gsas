e5fcbe5c17fa6806c0c24fa57578f8f62f3d42e7
Glenn <gmparagas1@up.edu.ph>
2025-11-05 11:37:34 +0800

    chore: changed database config

--- gsas/settings.py
+++ gsas/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'gsas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gsas_demo',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
