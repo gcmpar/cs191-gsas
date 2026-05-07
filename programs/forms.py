a0405b62a2d91e5350bac2d21e275d2c931a2284
Glenn <gmparagas1@up.edu.ph>
2026-05-07 15:26:18 +0800

    feat(programs): select2 widget for profile, form styling

--- programs/forms.py
+++ programs/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, Form, CharField, ModelChoiceField, widgets
+from django.forms import ModelForm, Form, CharField, ModelChoiceField
 from schools.models import School
 from schools.forms import SchoolsWidget
 from .models import Program
@@ -8,16 +8,13 @@ class ProgramForm(ModelForm):
         model = Program
         fields = ['school', 'program_name', 'description']
         widgets = {
-            'school':       widgets.Select(attrs={'class': 'form-select'}),
-            'program_name': widgets.TextInput(attrs={'class': 'form-control'}),
-            'description':  widgets.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'school': SchoolsWidget(
+                attrs={
+                    'data-placeholder': 'School',
+                    'data-minimum-input-length': 0
+                }
+            ),
         }
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # Show school names in the dropdown instead of the default "School object (1)"
-        self.fields['school'].queryset = School.objects.all()
-        self.fields['school'].label_from_instance = lambda obj: obj.school_name
 
 class ProgramsQueryForm(Form):
     search = CharField(required=False)
