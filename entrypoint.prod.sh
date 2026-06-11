f478aa7012cd6e530ed53b7f762186cfe159212d
Glenn <gmparagas1@up.edu.ph>
2026-06-11 13:39:37 +0800

    chore: nodocker, remove sh

--- entrypoint.prod.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-python manage.py collectstatic --noinput
-python manage.py migrate --noinput
-
-# NOTE: Django Select2 will break if workers > 1 ! Install and setup packages (e.g. redis) to fix.
-python -m gunicorn --bind 0.0.0.0:8000 --workers ${GUNICORN_WORKERS:-1} gsas.wsgi:application
